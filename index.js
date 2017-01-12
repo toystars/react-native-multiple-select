150a0c264bbe28831846025117a9b2dbe6b1e70c
Mustapha Babatunde <toystars2008@gmail.com>
2017-01-12 01:58:50 +0100

    Create component and link to index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+/*!
+ * react-native-multi-select
+ * Copyright(c) 2017 Mustapha Babatunde Oluwaleke
+ * MIT Licensed
+ */
+
+import MultiSelect from './lib/react-native-multi-select';
+export default MultiSelect;
