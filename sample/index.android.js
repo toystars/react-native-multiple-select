3c96224eb93ddeb89ac42e02e610a8ce2473c8e6
Mustapha Babatunde <toystars2008@gmail.com>
2017-05-12 23:52:20 +0100

    Update sample project

--- sample/index.android.js
+++ sample/index.android.js
@@ -9,23 +9,58 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
 } from 'react-native';
+import MultiSelect from 'react-native-multiple-select';
+
+const items = [{
+  id: '92iijs7yta',
+  name: 'Ondo',
+}, {
+  id: 'a0s0a8ssbsd',
+  name: 'C2G',
+}, {
+  id: '16hbajsabsd',
+  name: 'Calabar',
+}, {
+  id: 'nahs75a5sg',
+  name: 'Fourth Item',
+}, {
+  id: '667atsas',
+  name: 'Fifth Item',
+}];
+
+const selectedItem = (selectedItems) => {
+  // do something with selectedItems
+  console.log('Selected Items: ', selectedItems);
+};
 
 export default class sample extends Component {
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          MultiSelect Sample
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+
+        <MultiSelect
+          items={items}
+          uniqueKey="id"
+          selectedItemsChange={selectedItem}
+          selectedItems={[]}
+          selectText="Pick Items"
+          searchInputPlaceholderText="Search Items..."
+          altFontFamily="ProximaNova-Light"
+          tagRemoveIconColor="#CCC"
+          tagBorderColor="#CCC"
+          tagTextColor="#CCC"
+          selectedItemTextColor="#CCC"
+          selectedItemIconColor="#CCC"
+          itemTextColor="#000"
+          searchInputStyle={{ color: '#CCC' }}
+          submitButtonColor="red"
+          submitButtonText="Accept"
+        />
       </View>
     );
   }
@@ -34,8 +69,6 @@ export default class sample extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
   welcome: {
